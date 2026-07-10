aa8beb03fe909e41d88843b9d711895e888d7027
Paatsu <johnny@wong.se>
2026-07-10 18:59:30 +0200

    chore: Modify user-agent string for requests

--- tradera_cli/api.py
+++ tradera_cli/api.py
@@ -32,7 +32,7 @@ class TraderaClient:
         self.session.headers.update(
             {
                 "accept": "application/json, text/plain, */*",
-                "user-agent": "tradera-cli/0.1.1",
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
             }
         )
 
